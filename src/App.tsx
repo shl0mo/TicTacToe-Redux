bf477f259d2643b9b9e6aa69b3df8814b6bd6c35
shl0mo <salomao.dacostacruz@gmail.com>
2023-11-28 19:23:05 -0400

    Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Provider } from "react-redux";
 
 import { GameGrid } from "./components";
+import { Wrapper } from "./components/Wrapper";
 import store from "./store";
 
 function App () {
 	return (
 		<Provider store={store}>
-			<GameGrid/>
+			<Wrapper>
+				<GameGrid/>
+			</Wrapper>
 		</Provider>
 	)
 }
---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,16 @@
+export function Wrapper ({ children }) {
+    return (
+        <div style={wrapperStyle}>
+            {children}
+        </div>
+    )
+}
+
+const wrapperStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'rgba(0,0,0,0.95)'
+}
